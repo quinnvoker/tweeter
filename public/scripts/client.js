0d8b4e44058bce620d67732bbf629c8d2f76a099
quinnvoker <quinnvoker@gmail.com>
2020-08-12 08:09:30 -0700

    new tweets now sent via AJAX request

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -33,31 +33,14 @@ const renderTweets = (tweets) => {
   for (const tweet of tweets) {
     $('#timeline').append(createTweetElement(tweet));
   }
-}
+};
 
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
-
-renderTweets(data);
+$(document).ready(() => {
+  const $tweetForm = $('.new-tweet form');
+  $tweetForm.on('submit', (event) => {
+    event.preventDefault();
+    const queryString = $tweetForm.serialize();
+    $.ajax('/tweets', { method: 'POST', data: queryString });
+    $tweetForm.trigger('reset');
+  });
+});
