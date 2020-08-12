fee7110741fc276a519a5a8266450f5d1aaf4e32
quinnvoker <quinnvoker@gmail.com>
2020-08-12 07:20:23 -0700

    added createTweetElement function

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,27 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+const createTweetElement = (tweetData) => {
+  const markup = `
+  <article class="tweet">
+    <header>
+      <div class="user-display">
+        <img class="user-avatar" src="${tweetData.user.avatars}"> 
+        <span class="user-nickname">${tweetData.user.name}</span>
+      </div>
+      <span class="user-handle">${tweetData.user.handle}</span>
+    </header>
+    <span class="tweet-content">${tweetData.content.text}</span>
+    <footer>
+      <span class="post-time">${tweetData.created_at}</span>
+      <div class="actions">
+        <i class="fas fa-flag"></i>
+        <i class="fas fa-retweet"></i>
+        <i class="fas fa-heart"></i>
+      </div>
+    </footer>
+  </article>
+  `;
+
+  return $(markup);
+};
